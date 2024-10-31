7d9b7cf23122bdbfe666594067b7a000fc49b6b4
Vinicius-Martines-01 <vinicius.alcantarilla@fatec.sp.gov.br>
2024-10-30 23:48:22 -0300

    v2

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ function um_comment () {
     alert("Os Comentários não funcionam :(")
 }
 
+
+function writeComment (){
+    const com = document.getElementById('comments').value
+    const com_ul = document.querySelector('.commentarios')
+
+
+    let escrita = ''
+    escrita += `<div class="form-group mb-2" style="width: 100%;">
+                    <img src="img/vegeta.png" class="img_login" alt="avatar"/>
+                    <li class="um_com">${com}
+                </div>`;
+    com_ul.innerHTML += escrita;
+}
+
